51607fdfcba34085249299c5a199add0cc1eaf73
Brandon Simpson <brandonsimpson@gmail.com>
2015-11-24 16:46:23 -0800

    update path detection

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e # fail on any error
 
-NAME=${PWD##*/}
+ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
+DIR=$(dirname $ABSOLUTE_PATH)
+NAME=$(basename $DIR)
 
 docker-machine start $NAME
 docker-machine env $NAME
--- stop.sh
+++ stop.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e # fail on any error
 
-NAME=${PWD##*/}
+ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
+DIR=$(dirname $ABSOLUTE_PATH)
+NAME=$(basename $DIR)
 
 docker-machine stop $NAME
